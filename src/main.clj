8f1212bc8ee1e69b00eb938f783c959a351bc5a4
akshat <olttwa@users.noreply.github.com>
2022-04-20 14:01:57 +0530

    Solve medium problems

--- src/main.clj
+++ src/main.clj
@@ -10,15 +10,42 @@
 ((fn fibonacci
    ([n]
     (case n
-      0 ()
-      1 '(1)
-      2 '(1 1)
-      (fibonacci (- n 2) '(1 1))))
+      0 '()
+      1 [1]
+      2 [1 1]
+      (fibonacci (- n 2) [1 1])))
    ([n s]
     (if (< n 1)
       s
-      (fibonacci (- n 1) (concat s [(reduce + (take-last 2 s))]))))
+      (fibonacci (- n 1) (conj s (reduce + (take-last 2 s))))))
    )
- 8)
-; QQQ: How to do early return? Less indentation is clean code. But SO answers say early return is a bad idea.
-; QQQ: append/conjoin to end of list. conj is super confusing.
+ 4)
+
+; https://4clojure.oxal.org/#/problem/27
+; My solution.
+((fn palindrome?
+   [x]
+    (case (count x)
+       0 true
+       1 true
+       (if (= (first x) (last x))
+          (palindrome? (drop-last (rest x)))
+          false))
+    ) '(1 2 1))
+
+;QQQ: Given a sequence [1 2 3 4], how do I get a subset: [2 3]?
+; Simpler solution found online
+((fn [x] (= (reverse x)
+            (seq x))) "racecar")
+
+; QQQ: Explain https://4clojure.oxal.org/#/problem/51
+(=
+   [1 2 [3 4 5] [1 2 3 4 5]]
+   (let [[a b & c :as d] '( 1 2 3 4 5 )] [a b c d]))
+
+;https://4clojure.oxal.org/#/problem/99
+(= ((fn [x y] (map #(Character/digit % 10) (seq (str (* x y))))) 999 99) [9 8 9 0 1])
+(= ((fn [x y] (map (comp read-string str) (seq (str (* x y))))) 999 99) [9 8 9 0 1])
+
+; https://4clojure.oxal.org/#/problem/50
+((fn [x] (vals (group-by type x))) [1 :a 2 :b 3 :c])
